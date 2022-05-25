51a0ae1815c1a2e7cc70d63cc3978a821c2d7869
VladimirBludov <vladimir.bloodov@gmail.com>
2022-05-25 10:32:16 +0300

    fix controllers

--- controllers/contacts/changeStatus.js
+++ controllers/contacts/changeStatus.js
@@ -4,11 +4,12 @@ const { Contact } = require("../../models");
 const changeStatus = async (req, res) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
+  const { _id: userId } = req.user;
 
   if (favorite === undefined) throw new BadRequest("missing field favorite");
 
-  const updatedContact = await Contact.findByIdAndUpdate(
-    contactId,
+  const updatedContact = await Contact.findOneAndUpdate(
+    { owner: userId, _id: contactId },
     { $set: { favorite } },
     { new: true }
   );
